130442c69e04da0bf303ac9b035825d9136c2d0f
satdaug <95409141+satdaug@users.noreply.github.com>
2023-11-28 06:49:16 +0100

    Add files via upload

---
+++ src/preprocessing.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Libraries for general purpose
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Text cleaning
+import re
+import string
+import emoji
+import nltk
+import tqdm
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords
+
+# Data preprocessing
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler
+from langdetect import detect, LangDetectException
+import contractions
+from nltk.tokenize import word_tokenize
+
+
+# Define stop words for text cleaning
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+nltk.download('wordnet')
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+from nltk.probability import FreqDist
+from spacy.lang.en import English
+from spacy.lang.en.stop_words import STOP_WORDS
+# Initialize lemmatizer for text cleaning
+lemma = WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer()
+
+#Import Dataset
+df = pd.read_csv('../data-augmentation-commit-messages.csv', sep= ';', encoding='utf-8')
+df.head()
+
+# Remove punctuations, stopwords, links, mentions and new line characters
+def strip_all_entities(text):
+    text = re.sub(r'\r|\n', ' ', text.lower())  # Replace newline and carriage return with space, and convert to lowercase
+    text = re.sub(r"(?:\@|https?\://)\S+", "", text)  # Remove links and mentions
+    text = re.sub(r'[^\x00-\x7f]', '', text)  # Remove non-ASCII characters
+    banned_list = string.punctuation
+    table = str.maketrans('', '', banned_list)
+    text = text.translate(table)
+    text = ' '.join(word for word in text.split() if word not in stop_words)
+    return text
+
+# Clean hashtags at the end of the sentence, and keep those in the middle of the sentence by removing just the # symbol
+def clean_hashtags(tweet):
+    # Remove hashtags at the end of the sentence
+    new_tweet = re.sub(r'(\s+#[\w-]+)+\s*$', '', tweet).strip()
+    
+    # Remove the # symbol from hashtags in the middle of the sentence
+    new_tweet = re.sub(r'#([\w-]+)', r'\1', new_tweet).strip()
+    
+    return new_tweet
+
+# Filter special characters such as & and $ present in some words
+def filter_chars(text):
+    return ' '.join('' if ('$' in word) or ('&' in word) else word for word in text.split())
+
+# Remove multiple spaces
+def remove_mult_spaces(text):
+    return re.sub(r"\s\s+", " ", text)
+
+# Function to check if the text is in English, and return an empty string if it's not
+def filter_non_english(text):
+    try:
+        lang = detect(text)
+    except LangDetectException:
+        lang = "unknown"
+    return text if lang == "en" else ""
+
+# Expand contractions
+def expand_contractions(text):
+    return contractions.fix(text)
+
+# Remove numbers
+def remove_numbers(text):
+    return re.sub(r'\d+', '', text)
+
+# Lemmatize words
+def lemmatize(text):
+    words = word_tokenize(text)
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+    return ' '.join(lemmatized_words)
+
+# Remove short words
+def remove_short_words(text, min_len=2):
+    words = text.split()
+    long_words = [word for word in words if len(word) >= min_len]
+    return ' '.join(long_words)
+
+# Replace elongated words with their base form
+def replace_elongated_words(text):
+    regex_pattern = r'\b(\w+)((\w)\3{2,})(\w*)\b'
+    return re.sub(regex_pattern, r'\1\3\4', text)
+
+# Remove repeated punctuation
+def remove_repeated_punctuation(text):
+    return re.sub(r'[\?\.\!]+(?=[\?\.\!])', '', text)
+
+# Remove extra whitespace
+def remove_extra_whitespace(text):
+    return ' '.join(text.split())
+
+def remove_url_shorteners(text):
+    return re.sub(r'(?:http[s]?://)?(?:www\.)?(?:bit\.ly|goo\.gl|t\.co|tinyurl\.com|tr\.im|is\.gd|cli\.gs|u\.nu|url\.ie|tiny\.cc|alturl\.com|ow\.ly|bit\.do|adoro\.to)\S+', '', text)
+
+# Remove spaces at the beginning and end of the tweet
+def remove_spaces_tweets(tweet):
+    return tweet.strip()
+
+# Remove short tweets
+def remove_short_tweets(tweet, min_words=3):
+    words = tweet.split()
+    return tweet if len(words) >= min_words else ""
+
+# Function to call all the cleaning functions in the correct order
+def clean_text(tweet):
+    #tweet = strip_emoji(tweet)
+    tweet = expand_contractions(tweet)
+    tweet = filter_non_english(tweet)
+    tweet = strip_all_entities(tweet)
+    tweet = clean_hashtags(tweet)
+    tweet = filter_chars(tweet)
+    tweet = remove_mult_spaces(tweet)
+    tweet = remove_numbers(tweet)
+    tweet = lemmatize(tweet)
+    tweet = remove_short_words(tweet)
+    tweet = replace_elongated_words(tweet)
+    tweet = remove_repeated_punctuation(tweet)
+    tweet = remove_extra_whitespace(tweet)
+    tweet = remove_url_shorteners(tweet)
+    tweet = remove_spaces_tweets(tweet)
+    #tweet = remove_short_tweets(tweet)
+    tweet = ' '.join(tweet.split())  # Remove multiple spaces between words
+    return tweet
+
+df.to_csv('commit_augmented.csv', encoding='utf-8', index=False)
+
